f2d5fd83129e63379f2ec44dbd70f8f32a6ebb50
Mauricio Monroy <mauricio.monroy-j@mail.escuelaing.edu.co>
2024-03-08 08:47:12 -0500

    parte IV

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -3,7 +3,11 @@ package com.example.servingwebcontent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class GreetingController {
@@ -12,6 +16,20 @@ public class GreetingController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+
+    @RequestMapping("/task/{id}")
+    private ModelAndView  getUser(@PathVariable Integer id, Model model) {
+        String uri = "https://jsonplaceholder.typicode.com/todos/" + id;
+        RestTemplate restTemplate = new RestTemplate();
+
+        Task task = restTemplate.getForObject(uri, Task.class);
+
+        ModelAndView modelAndView = new ModelAndView("task");
+        modelAndView.addObject("task", task);
+
+        return modelAndView;
     }
 
 }
